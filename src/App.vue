dc6a2502d777dfb6f0fcd9408e0de6be704e49e6
Joel <joel.garplind@gmail.com>
2017-03-20 22:00:11 +0100

    setup basics

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      countries: [],
+      countriesLocation: 'https://restcountries.eu/rest/v2'
+    }
+  },
+
+  methods () {
+    fetchCountries() {
+      fetch(this.countriesLocation)
+          .then(blob => blob.json())
+          .then(data => this.countries.push(...data));
+      // this.pushCities();
+      // this.pushProducts();
+    },
+
+  },
+
+  beforeMount() {
+    this.fetchBalances()
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
