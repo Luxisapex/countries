2146c7833666ca9622bf51884635b3f53b93aed0
Joel <joel.garplind@gmail.com>
2017-04-07 22:25:37 +0200

    included express

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const http = require('http')
-const port = process.env.PORT || 3000;
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node Server!');
-}
-
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('badness', err);
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+var express = require(‘express’);
+var path = require(‘path’);
+var serveStatic = require(‘serve-static’)
+app = express();
+app.use(serveStatic(path.join(__dirname, ‘dist’)));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log(‘server started ‘+port);
